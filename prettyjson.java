b1a25d6faed00ce73ad12d32099162fd9774e5ca
pankypal <pankajpal.dce@gmail.com>
2015-09-28 01:38:11 +0530

    Update prettyjson.java

--- prettyjson.java
+++ prettyjson.java
@@ -1,8 +1,8 @@
-Pretty print a json object using proper indentation.
+/*Pretty print a json object using proper indentation.
 
 Every inner brace should increase one indentation to the following lines.
 Every close brace should decrease one indentation to the same line and the following lines.
-The indents can be increased with an additional ë\tí
+The indents can be increased with an additional ‚Äò\t‚Äô
 Example 1:
 
 Input : {A:"B",C:{D:"E",F:{G:"H",I:"J"}}}
@@ -39,7 +39,9 @@ Output :
 
 Assume for this problem that space characters can be done away with.
 
-Your solution should return a list of strings, where each entry corresponds to a single line. The strings should not have ì\nî character in them.
+Your solution should return a list of strings, where each entry corresponds to a single line. The strings should not have ‚Äú\n‚Äù character in them.
+*/
+
 
 public class Solution {
 	public ArrayList<String> prettyJSON(String a) {
